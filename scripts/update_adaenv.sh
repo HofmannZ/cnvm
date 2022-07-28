5023e654c8ba14150c64e37f7674868313df7b0b
Zino Hofmann <zino@hofmann.amsterdam>
2022-07-28 13:06:10 +0000

    feat: keep the NODE_CONFIG and NODE_PORT

--- scripts/update_adaenv.sh
+++ scripts/update_adaenv.sh
@@ -1,5 +1,14 @@
+echo "💾 Saving partial config..."
+CURRENT_NODE_CONFIG=$NODE_CONFIG
+CURRENT_NODE_PORT=$NODE_PORT
+
 echo "📂 Copying adaenv..."
 cp $DOTFILES/.adaenv ~/.adaenv
+
+echo "✅ Restoring partial config..."
+sed -i ${HOME}/.adaenv \
+    -e "s/NODE_CONFIG.*/NODE_CONFIG=${CURRENT_NODE_CONFIG}/g" \
+    -e "s/NODE_PORT.*/NODE_PORT=${CURRENT_NODE_PORT}/g"
 
 echo "📂 Sourcing adaenv..."
 source ~/.adaenv
