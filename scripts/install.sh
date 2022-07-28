12e35df5834fe00681956e05844a92f3074d4161
Zino Hofmann <zino@hofmann.amsterdam>
2022-07-28 13:13:28 +0000

    feat: support adaenv path override

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,2 @@
 . $HOME/git/cardano-spo-tools/scripts/install_deps.sh
-. $HOME/git/cardano-spo-tools/scripts/update_adaenv.sh
+. $HOME/git/cardano-spo-tools/scripts/update_adaenv.sh $HOME/git/cardano-spo-tools/.adaenv
--- scripts/update_adaenv.sh
+++ scripts/update_adaenv.sh
@@ -1,12 +1,20 @@
+ADAENV_PATH=$1
+
+if (
+    [ -z "${ADAENV_PATH}" ]
+); then
+    ADAENV_PATH=$CARDANO_SPO_TOOLS/.adaenv
+fi
+
 echo "💾 Saving config..."
 CURRENT_NODE_CONFIG=$NODE_CONFIG
 CURRENT_NODE_PORT=$NODE_PORT
 
 echo "📂 Copying adaenv..."
-cp $CARDANO_SPO_TOOLS/.adaenv ~/.adaenv
+cp $ADAENV_PATH ~/.adaenv
 
 echo "✅ Restoring config..."
-sed -i ${HOME}/.adaenv \
+sed -i ~/.adaenv \
     -e "s/NODE_CONFIG.*/NODE_CONFIG=${CURRENT_NODE_CONFIG}/g" \
     -e "s/NODE_PORT.*/NODE_PORT=${CURRENT_NODE_PORT}/g"
 
