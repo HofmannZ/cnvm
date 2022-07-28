4ab67542faa640e4d0b2243dbae559c96bc5626c
Zino Hofmann <zino@hofmann.amsterdam>
2022-07-28 19:00:42 +0000

    chore: colors!

--- scripts/update_adaenv.sh
+++ scripts/update_adaenv.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+# colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+red() {
+    printf "${RED}$@${NC}\n"
+}
+green() {
+    printf "${GREEN}$@${NC}\n"
+}
+yellow() {
+    printf "${YELLOW}$@${NC}\n"
+}
+
 ADAENV_PATH=$1
 
 if (
@@ -7,19 +24,19 @@ if (
     ADAENV_PATH=$CARDANO_SPO_TOOLS/.adaenv
 fi
 
-echo "💾 Saving config..."
+echo $(green "💾 Saving config...")
 CURRENT_NODE_CONFIG=$NODE_CONFIG
 CURRENT_NODE_PORT=$NODE_PORT
 CURRENT_NODE_BUILD_NUM=$NODE_BUILD_NUM
 
-echo "📂 Copying .adaenv..."
+echo $(green "📂 Copying .adaenv...")
 cp $ADAENV_PATH ~/.adaenv
 
-echo "✅ Restoring config..."
+echo $(green "✅ Restoring config...")
 sed -i ~/.adaenv \
     -e "s/NODE_CONFIG=.*/NODE_CONFIG=${CURRENT_NODE_CONFIG}/g" \
     -e "s/NODE_PORT=.*/NODE_PORT=${CURRENT_NODE_PORT}/g" \
     -e "s/NODE_BUILD_NUM=.*/NODE_BUILD_NUM=${CURRENT_NODE_BUILD_NUM}/g"
 
-echo "📡 Sourcing .adaenv..."
+echo $(green "📡 Sourcing .adaenv...")
 source ~/.adaenv
--- scripts/update_self.sh
+++ scripts/update_self.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
-echo "📋 Updating Cardano spo tools..."
 
-echo "💾 Saving directory..."
+# colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+red() {
+    printf "${RED}$@${NC}\n"
+}
+green() {
+    printf "${GREEN}$@${NC}\n"
+}
+yellow() {
+    printf "${YELLOW}$@${NC}\n"
+}
+
+echo $(green "📋 Updating Cardano spo tools...")
+
+echo $(green "💾 Saving directory...")
 CURRRENT_DIR=$(pwd)
 
-echo "📂 Pulling latest changes..."
+echo $(green "📂 Pulling latest changes...")
 cd $CARDANO_SPO_TOOLS
 git pull
 . ./scripts/update_adaenv.sh
 
-echo "✅ Restoring directory..."
+echo $(green "✅ Restoring directory...")
 cd $CURRRENT_DIR
 
-echo "✅ All done!"
+echo $(green "✅ All done!")
